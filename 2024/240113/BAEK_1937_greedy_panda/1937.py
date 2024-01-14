f47609445eb030c3c38b0b1db569c0633dd7850b
julsukim <julsukim96@gmail.com>
2024-01-15 01:00:15 +0900

    240114

--- 2024/240113/BAEK_1937_greedy_panda/1937.py
+++ 2024/240113/BAEK_1937_greedy_panda/1937.py
@@ -5,44 +5,34 @@
 # 1 <= N <= 500
 # 대나무 갯수 <= 1000000
 # 판다의 최대 이동 횟수 출력
-from collections import deque
 import sys
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+
+def dfs(i, j):
+    if dp[i][j] == 0:
+        dp[i][j] = 1
+
+        for di, dj in delta:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < N and 0 <= nj < N:
+                if forest[ni][nj] > forest[i][j]:
+                    dp[i][j] = max(dp[i][j], dfs(ni, nj) + 1)
+
+    return dp[i][j]
+
 
 N = int(input())
 forest = [list(map(int, input().split())) for _ in range(N)]
 
+dp = [[0] * N for _ in range(N)]
 delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-print(forest)
 max_move = 0
 
 for i in range(N):
     for j in range(N):
-        visited = [[0 for _ in range(N)] for _ in range(N)]
-
-        dq = deque()
-        start_point = (i, j)
-        dq.append(start_point)
-        visited[i][j] = 1
-
-        while dq:
-            ci, cj = dq.popleft()
-            for di, dj in delta:
-                ni, nj = ci+di, cj+dj
-                if 0 <= ni < N and 0 <= nj < N:
-                    if forest[ci][cj] < forest[ni][nj]:
-                        visited[ni][nj] = visited[ci][cj] + 1
-                        dq.append((ni, nj))
-        move = 0
-        for i in range(N):
-            for j in range(N):
-                if move < visited[i][j]:
-                    move = visited[i][j]
-        if move > max_move:
-            max_move = move
-            print(visited)
+        max_move = max(max_move, dfs(i, j))
 
 print(max_move)
-
-
