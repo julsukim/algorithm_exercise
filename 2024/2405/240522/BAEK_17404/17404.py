af40d2ce537ca8507e6d379e2d1a94422493b60b
김준수 <pittuarez@naver.com>
2024-05-23 09:34:13 +0900

    solve: 17404

--- 2024/2405/240522/BAEK_17404/17404.py
+++ 2024/2405/240522/BAEK_17404/17404.py
@@ -1,12 +1,21 @@
 N = int(input())
 rgb_costs = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0] * 3 for _ in range(N)]
-dp[0] = rgb_costs[0]
+INF = 10e9
+answer = INF
 
-for i in range(1, N):
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb_costs[i][0]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb_costs[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb_costs[i][2]
+for start in range(3):
+    dp = [[0] * 3 for _ in range(N)]
+    dp[0] = [INF, INF, INF]
 
-print(min(dp[N-1][0], dp[N-1][1], dp[N-1][2]))
+    dp[0][start] = rgb_costs[0][start]
+
+    for i in range(1, N):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb_costs[i][0]
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb_costs[i][1]
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb_costs[i][2]
+
+    dp[N-1][start] = INF
+    answer = min(answer, min(dp[N-1]))
+
+print(answer)
