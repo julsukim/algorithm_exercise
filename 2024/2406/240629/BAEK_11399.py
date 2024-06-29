b550d6c48e963dde27045d359abc53681b7f9335
김준수 <julsukim96@gmail.com>
2024-06-29 14:43:49 +0900

    solve: 11399

---
+++ 2024/2406/240629/BAEK_11399.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int, input().split()))
+P.sort()
+result = 0
+for i in range(len(P)):
+    result += sum(P[:i+1])
+print(result)
