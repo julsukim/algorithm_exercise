62f44952a79c552d574db5b857bbe24e694219a0
김준수 <julsukim96@gmail.com>
2024-07-04 21:56:55 +0900

    solve: 9251

---
+++ Python/2024/2407/240704/BAEK_9251.py
@@ -0,0 +1,34 @@
+# import sys
+# input = sys.stdin.readline
+#
+# N = input().rstrip()
+# M = input().rstrip()
+# print(len(N), len(M))
+#
+# dp = [[0 for _ in range(len(M)+1)] for _ in range(len(N)+1)]
+#
+# for i in range(1, len(N)+1):
+#     for j in range(1, len(M)+1):
+#         if N[i-1] == M[j-1]:
+#             dp[i][j] = dp[i-1][j-1] + 1
+#         else:
+#             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+#
+# print(dp)
+# print(dp[len(N)][len(M)])
+
+a = list(str(input()))
+b = list(str(input()))
+
+dp = [[0 for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp)
+print(dp[len(a)][len(b)])
+
