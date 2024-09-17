6af5384860562ab2572512a9fddb488d9b4853f9
김준수 <julsukim96@gmail.com>
2024-09-17 14:34:52 +0900

    solve: 10973

---
+++ Python/2024/2409/240917/BAEK_10973.py
@@ -0,0 +1,18 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+i = N - 1
+while i > 0 and arr[i - 1] <= arr[i]:
+    i -= 1
+
+if i == 0:
+    print(-1)
+else:
+    j = N - 1
+    while arr[j] >= arr[i - 1]:
+        j -= 1
+
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+
+    arr[i:] = reversed(arr[i:])
+    print(*arr)
